a7e3e4029b7d0bb2e0faf840909dcd1852c10b03
Simon Hoss <Simon.Hoss@gmx.ch>
2021-01-21 22:17:11 +0100

    correct type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,27 @@
-import type { JSEvalable } from "puppeteer";
+export type EvaluateFn<T = any> = string | ((arg1: T, ...args: any[]) => any);
+export type EvaluateFnReturnType<T extends EvaluateFn> = T extends (
+  ...args: any[]
+) => infer R
+  ? R
+  : unknown;
+
+export interface JSEvalable<A = any> {
+  /**
+   * Evaluates a function in the browser context.
+   * If the function, passed to the frame.evaluate, returns a Promise, then frame.evaluate would wait for the promise to resolve and return its value.
+   * If the function passed into frame.evaluate returns a non-Serializable value, then frame.evaluate resolves to undefined.
+   * @param fn Function to be evaluated in browser context
+   * @param args Arguments to pass to `fn`
+   */
+  evaluate<T extends EvaluateFn<A>>(
+    pageFunction: T,
+    ...args: any[]
+  ): Promise<
+    EvaluateFnReturnType<T> extends PromiseLike<infer U>
+      ? U
+      : EvaluateFnReturnType<T>
+  >;
+}
 
 export function toMatchElementStyleSnapshotFromPage(
   page: JSEvalable
